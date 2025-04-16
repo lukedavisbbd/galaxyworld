4dadf85ed1f9883de99abcb69c108a1bc2574539
Luke Davis <luke.davis@sanlam.co.za>
2025-04-16 22:01:41 +0200

    allow egress

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,13 +35,22 @@ resource "aws_default_subnet" "subnet_az2" {
 }
 
 resource "aws_security_group" "allow_postgres" {
-  name_prefix = "allow_postgress"
+  name_prefix = "allow_postgres"
 
   ingress {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
   }
 }
 
